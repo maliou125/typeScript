383536111f43553190c14e7ca40655f02b11de24
marwen maliou <158064032+maliou125@users.noreply.github.com>
2024-07-03 17:48:42 +0100

    typeScript
    1.0

---
+++ car.ts
@@ -0,0 +1,18 @@
+class Car implements Vehicle {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    start(): void {
+        console.log("Car engine started");
+    }
+}
+
+const myCar = new Car('Toyota', 'Corolla', 2020);
+myCar.start();
---
+++ vehicle.ts
@@ -0,0 +1,6 @@
+interface Vehicle {
+    make: string;
+    model: string;
+    year: number;
+    start(): void;
+}
